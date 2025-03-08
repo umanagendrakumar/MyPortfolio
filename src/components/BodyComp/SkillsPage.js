3d4a471a3bece65f7f3eb5305886064cf0a33d02
umanagendrakumar <your.email@example.com>
2025-03-09 00:21:48 +0530

    skills added and grid is used

--- src/components/BodyComp/SkillsPage.js
+++ src/components/BodyComp/SkillsPage.js
@@ -1,11 +1,36 @@
 
 const Skills = () => {
     return (
-        <div className="body">
-            <h1>Iam Skills</h1>
+        <div className="skills cover80" id="skills">
+            <div className="skills-wrapper">
+                <div className="skills-head">
+                    Skills
+                </div>
+                <div className="skills-hub">
+                    <div className="skill-card dev-card">Development  &rarr;</div>
+                    <div className="skill-card html-card">HTML</div>
+                    <div className="skill-card java-card">Java</div>
+                    <div className="skill-card css-card">CSS</div>
+                    <div className="skill-card js-card">JavaScript</div>
+                    <div className="skill-card frame-card">&larr;  Frameworks</div>
+                    <div className="skill-card tcss-card">Tailwind CSS</div>
+                    <div className="skill-card rjs-card">ReactJS</div>
+                    <div className="skill-card vcs-card">VCS  &rarr;</div>
+                    <div className="skill-card git-card">Git</div>
+                    <div className="skill-card github-card">GitHub</div>
+                    <div className="skill-card gitlab-card">GitLab</div>
+                    <div className="skill-card dsa-card">DSA</div>
+                    <div className="skill-card sql-card">SQL</div>
+                    <div className="skill-card dbms-card">DBMS</div>
+                    <div className="skill-card os-card">OS</div>
+                    <div className="skill-card cn-card">CN</div>
+                </div>
+
+
+            </div>
         </div>
-    );            
-    
+    );
+
 };
 
 export default Skills;
