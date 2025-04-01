5d4e9096ce0bd1cdebb46bc59eaad091fa4c3239
umanagendrakumar <your.email@example.com>
2025-04-01 23:53:50 +0530

    one project added

--- src/components/BodyComp/ProjectsPage.js
+++ src/components/BodyComp/ProjectsPage.js
@@ -1,12 +1,21 @@
 
+import Card from "../../UIComponents/projectCard";
+import { projectList } from "../../UIComponents/projectCard";
 
 const Projects = () => {
     return (
         <div className="projects cover80">
-            <h1>Iam Projects</h1>
+            <h1 className="project-heading">Projects</h1>
+            <div className="project-cards">
+            {
+                projectList.map((eachProj) => {
+                    return <Card key={eachProj.index} {...eachProj} />
+                })
+            }
+            </div>
         </div>
     );
-           
+
 };
 
 export default Projects;
