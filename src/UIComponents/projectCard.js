a3ec50c60e87521a0f1955c5e337678d9992098c
umanagendrakumar <your.email@example.com>
2025-04-04 20:50:15 +0530

    title moves to top

--- src/UIComponents/projectCard.js
+++ src/UIComponents/projectCard.js
@@ -1,26 +1,27 @@
 import gradeConverterImg from "../../assets/proj-gpaConverter.png";
 import todoListImg from "../../assets/proj-todoList.png";
+// import portfolioImg from "../../assets/proj-portfolio.png";
 import portfolioImg from "../../assets/proj-portfolio.png";
 
 
 export const projectList = [{
     index: 1,
     imgUrl: gradeConverterImg,
-    title: "GPA Converter",
+    title: "GPA CONVERTER",
     liveUrl: "https://uma03.netlify.app/",
     githubUrl: "https://github.com/umanagendrakumar/5thSemester-Grade-to-SGPA"
 },
 {
     index: 2,
     imgUrl: todoListImg,
-    title: "To-Do List With Bin",
+    title: "T0-DO LIST WITH BIN",
     liveUrl: "https://uma-todolist.netlify.app/",
     githubUrl: "https://github.com/umanagendrakumar/Todo_List"
 },
 {
     index: 3,
     imgUrl: portfolioImg,
-    title: "My Portfolio",
+    title: "MY PORTFOLIO",
     liveUrl: "https://kunk-portfolio.netlify.app/",
     githubUrl: "https://github.com/umanagendrakumar/MyPortfolio"
 }
@@ -30,13 +31,13 @@ export const projectList = [{
 const Card = ({ imgUrl, title, liveUrl, githubUrl }) => {
     return (
         <div className="projectCard">
+            <div className="title">{title}</div>
             <div className="img-wrapper">
                 <img src={imgUrl} className="img" alt="Project image"></img>
             </div>
-            <div className="title">{title}</div>
             <div className="links-wrapper">
-                <a href={liveUrl} className="link live-btn">Live</a>
-                <a href={githubUrl} className="link github-btn">Github</a>
+                <a href={liveUrl} className="link live-btn">LIVE</a>
+                <a href={githubUrl} className="link github-btn">GITHUB</a>
             </div>
         </div>
     );
