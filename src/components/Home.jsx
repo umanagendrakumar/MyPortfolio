05146f919ffb4be4623360b13c9aab19cebbbb52
umanagendrakumar <your.email@example.com>
2025-08-07 21:26:04 +0530

    Image added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { SiLeetcode } from 'react-icons/si';
+import Photo from "../assets/Photo.png"
 
 const Home = () => {
     return (
@@ -7,15 +8,15 @@ const Home = () => {
             <div className="flex flex-col md:flex-row justify-center items-center gap-4 my-auto">
                 <figure className="">
                     <img
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL2FQ72FWEtzxniuTGYHMNDjFiJ0p2ULQELg&s"
+                        src={Photo}
                         alt="Profile pic"
-                        className="rounded-[50%] p-4" />
+                        className="rounded-[50%] p-4 h-80 w-80" />
                 </figure>
                 <div className="card-body max-w-[450px] border-l">
                     <h2 className="card-title text-2xl">Hello, I'm</h2>
                     <h2 className="card-title text-2xl text-violet-400">Kambala Uma Nagendra Kumar</h2>
                     <p className="text-xl">Aspiring Developer</p>
-                    <p className="text-md">Front-End Developer | Back-End Developer | Full Stack Developer</p>
+                    <p className="text-md">Full Stack Developer | Software Developer</p>
                     <p className="text-xs text-justify hidden sm:block">I am always eager to collaborate with like-minded professionals, take on challenging projects, and stay updated with the latest advancements in modern technologies.</p>
                     <div className="card-actions mt-4">
                         <a href="https://www.linkedin.com/in/kunk/" className="text-2xl mr-4"><FaLinkedin /></a>
@@ -25,7 +26,7 @@ const Home = () => {
 
                 </div>
             </div>
-            
+
 
         </main>
     );
