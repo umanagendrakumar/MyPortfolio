2e8dccd4aac8d332ebd185285f7a9149c0c2aaf3
umanagendrakumar <your.email@example.com>
2025-11-28 22:18:15 +0530

    Resume & learning updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { FiDownload, FiUser } from 'react-icons/fi'
 import { MdPerson } from 'react-icons/md';
-import resume from "../assets/UmaNagendra_Resume.pdf";
+import resume from "../assets/UmaNagendraKumar_Resume.pdf";
 
 const About = () => {
     return (
@@ -11,7 +11,7 @@ const About = () => {
                     <h2 className="card-title text-2xl mb-2">Who am I ?</h2>
                     <p className="text-sm">Hey there! I’m a recent Computer Science Engineering Graduate from Acharya Nagarjuna University with a deep passion for solving real-world problems through code.
 
-                        Over time, I’ve developed a strong foundation in the <span className=' text-violet-300 font-bold'>Core Java, SQL, JavaScript and MERN Stack </span> and version control tools like <span className=' text-violet-300 font-bold'>Git & GitHub</span>. I'm also sharpening my problem-solving skills by diving deep into <span className=' text-violet-300 font-bold'>Data Structures and Algorithms (DSA)</span> — because I believe writing efficient code is just as important as writing functional code.
+                        Over time, I’ve developed a strong foundation in the <span className=' text-violet-300 font-bold'>Core Java, SQL, JavaScript</span> and version control tools like <span className=' text-violet-300 font-bold'>Git & GitHub</span>. Currently learning <span className=' text-violet-300 font-bold'>SpringBoot </span>. I'm also sharpening my problem-solving skills by diving deep into <span className=' text-violet-300 font-bold'>Data Structures and Algorithms (DSA)</span> — because I believe writing efficient code is just as important as writing functional code.
 
                         <br></br> <br></br>Beyond academics, I thrive on continuous learning, and contributing to projects that challenge me to grow. I am always eager to collaborate with like-minded professionals, take on challenging projects, and stay updated with the latest advancements in modern technologies.
 
