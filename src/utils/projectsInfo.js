7be1dc09c856a82bf7f8aa7650fd2e0148e02102
umanagendrakumar <your.email@example.com>
2026-01-25 19:16:44 +0530

    Updated Tech Stack

--- src/utils/projectsInfo.js
+++ src/utils/projectsInfo.js
@@ -13,7 +13,7 @@ export const projectsInfo = [
         imageUrl: devcharge,
         title: "DevCharge",
         description: "A real-time matchmaking platform where developers can discover and connect with like-minded peers, inspired by Tinder, but built for devs.",
-        tech: "Springboot + React.js",
+        tech: "Node.js + React.js",
         links: {
             active: true,
             live: "https://devcharge.netlify.app/",
