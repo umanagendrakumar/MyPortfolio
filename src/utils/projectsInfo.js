30b0f18ef3517897de3ce02dd36fe607549e725b
umanagendrakumar <your.email@example.com>
2026-03-15 16:52:27 +0530

    New Project Added and some removed

--- src/utils/projectsInfo.js
+++ src/utils/projectsInfo.js
@@ -6,10 +6,23 @@ import converterImg from "../assets/converter.png"
 import ultraEditClone from "../assets/ultraEditClone.png" 
 import todoList from "../assets/todoList.png"
 import profileCard from "../assets/profileCard.png"
+import orbitra from "../assets/orbitra.png"
 
 export const projectsInfo = [
     {
         id: "project1",
+        imageUrl: orbitra,
+        title: "Orbitra",
+        description: "A centralized job application tracking system for Job Seekers. It offers a user-friendly interface to manage job applications, track progress, and stay organized.",
+        tech: "Spring Boot + React.js",
+        links: {
+            active: true,
+            live: "https://project-orbit-alpha.vercel.app/",
+            github: "https://github.com/umanagendrakumar/Project-Orbitra-Backend",
+        },
+    },
+    {
+        id: "project2",
         imageUrl: devcharge,
         title: "DevCharge",
         description: "A real-time matchmaking platform where developers can discover and connect with like-minded peers, inspired by Tinder, but built for devs.",
@@ -21,7 +34,7 @@ export const projectsInfo = [
         },
     },
     {
-        id: "project2",
+        id: "project3",
         imageUrl: netflicGPT,
         title: "NetflicGPT",
         description: "A Netflix clone integrated with OpenAI's GPT, allowing users to search for movies and get recommendations based on their preferences.",
@@ -33,7 +46,7 @@ export const projectsInfo = [
         },
     },
     {
-        id: "project3",
+        id: "project4",
         imageUrl: biteFood,
         title: "BiteFood",
         description: "BiteFood is a mini food web application inspired by Swiggy. This project is a work in progress and is helping me strengthen my React development skills.",
@@ -46,7 +59,7 @@ export const projectsInfo = [
          extra4Proj3: true,
     },
     {
-        id: "project4",
+        id: "project5",
         imageUrl: portfolio,
         title: "Portfolio",
         description: "A personal portfolio website built with React and Tailwind CSS to showcase my projects, skills, and experience. It reflects my development style, UI design approach.",
@@ -58,7 +71,7 @@ export const projectsInfo = [
         },
     },
     {
-        id: "project5",
+        id: "project6",
         imageUrl: converterImg,
         title: "GPA Converter",
         description: "A GPA Converter built using HTML, CSS, and JavaScript to solve a real problem faced by me and my friends. It quickly converts grades to SGPA using a simple, user-friendly interface.",
@@ -70,19 +83,6 @@ export const projectsInfo = [
         },
     },
     {
-        id: "project6",
-        imageUrl: ultraEditClone,
-        title: "UltraEdit Clone",
-        description: "A responsive UltraEdit clone built with HTML and CSS, featuring a clean and user-friendly interface that adapts smoothly to different screen sizes.",
-        tech: "HTML, CSS",
-        links: {
-            active: true,
-            live: "https://uma-ultraedit.netlify.app/",
-            github: "https://github.com/umanagendrakumar/UltraEdit_Clone",
-        },
-        extra4Proj6: true
-    },
-    {
         id: "project7",
         imageUrl: todoList,
         title: "Todo List",
@@ -92,18 +92,6 @@ export const projectsInfo = [
             active: true,
             live: "https://uma-todolist.netlify.app/",
             github: "https://github.com/umanagendrakumar/Todo_List",
-        },
-    },
-    {
-        id: "project8",
-        imageUrl: profileCard,
-        title: "Profile Card",
-        description: "A responsive profile card built using HTML and CSS. This was my first frontend project helping me understand layout, styling. A Small try haha...",
-        tech: "HTML, CSS",
-        links: {
-            active: true,
-            live: "https://uma-card.netlify.app/",
-            github: "https://github.com/umanagendrakumar/My-Profile-Card",
         },
     },
 ]
